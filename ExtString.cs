5c9520f0e489fab9aaf8f0820230724a5eb0ae05
wilsoncao <f.yycaf@gmail.com>
2013-06-16 02:58:02 +0800

    t
    t

---
+++ ExtString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToolHelp.Extension
+{
+    public static class ExtString
+    {
+        public static string ToMiniImgUrl(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return "";
+            return str.Replace("original", "mini");
+        }
+        public static string ToMicroImgUrl(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return "";
+            return str.Replace("original", "micro");
+        }
+        public static string ToShortTitle(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return "";
+            return str.Substring(0, 40)+"...";
+        }
+    }
+}
